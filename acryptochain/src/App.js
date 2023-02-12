f34e65b643a705089a168a4e2048117116d24361
kushalzone <kushalzone@gmail.com>
2023-02-12 14:15:29 -0500

    Added UI to show the token info

--- acryptochain/src/App.js
+++ acryptochain/src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import * as React from 'react';
 import Web3 from 'web3';
 import './App.css';
 import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
+import Container from '@mui/material/Container';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const App = () => {
   const [address, setAddress] = useState('0xc7981767f644C7F8e483DAbDc413e8a371b83079');
@@ -11,6 +20,7 @@ const App = () => {
   const [name, setName] = useState(null);
   const [symbol, setSymbol] = useState(null);
   const [decimals, setDecimals] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
 
   const handleInputChange = event => {
@@ -35,58 +45,81 @@ const App = () => {
     getSymbol(contract, setSymbol)
     getDecimals(contract, setDecimals)
 
+    setLoaded(true)
+
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Contract Address:
-          <Input size='48' value={address} onChange={handleInputChange} />
-        </label>
-        <Button variant="contained" type="submit">Find Info</Button>
-      </form>
+    <Container>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h1>Token Info Fetcher</h1>
+          <label>
+            <p >Enter token contract address:</p>
+            <Input value={address} onChange={handleInputChange} fullWidth={true} />
+          </label>
+          <p/>
+          <Button variant="contained" type="submit">Find Info</Button>
+        </form>
+        <br/>
+      </div>
 
-      <p>Token Detail:</p>
+{loaded &&
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 450, tableLayout: "auto", background:"white"}}aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell><b>Item</b></TableCell>
+              <TableCell align="right"><b>Value</b></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            
+          <TableRow>
+              <TableCell align="left">{'Token Address'}</TableCell>
+              <TableCell align="right">{address}</TableCell>
+            </TableRow>
 
-      {name && <p>{name}</p>}
-      {symbol && <p>{symbol}</p>}
-      {decimals && <p>{decimals}</p>}
-      {totalSupply && <p>{totalSupply}</p>}
-      {owner && <p>{owner}</p>}
-    </div>
+            <TableRow>
+              <TableCell align="left">{'Name'}</TableCell>
+              <TableCell align="right">{name}</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell align="left">{'Symbol'}</TableCell>
+              <TableCell align="right">{symbol}</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell align="left">{'Total Supply'}</TableCell>
+              <TableCell align="right">{totalSupply}</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell align="left">{'Decimals'}</TableCell>
+              <TableCell align="right">{decimals}</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell align="left">{'Owner'}</TableCell>
+              <TableCell align="right">{owner}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    }
+    </Container>
+
   );
 };
 
-function getTotalSupply(contract, web3, setContractResponse) {
-  contract.methods.totalSupply().call((error, result) => {
-    if (error) {
-      console.error(error);
-    } else {
-      const resultInEther = web3.utils.fromWei(result, 'ether');
-      console.log(`Total Supply ${resultInEther}`);
-      setContractResponse("\nTotal Supply: " + resultInEther);
-    }
-  });
-}
-
-function getOwner(contract, setOwner) {
-  contract.methods.owner().call((error, result) => {
-    if (error) {
-      console.error(error);
-    } else {
-      setOwner("\nOwner: " + result);
-    }
-
-  });
-}
 
 function getName(contract, setName) {
   contract.methods.name().call((error, result) => {
     if (error) {
       console.error(error);
     } else {
-      setName("\nName: " + result);
+      setName(result);
     }
 
   });
@@ -95,21 +128,45 @@ function getName(contract, setName) {
 function getSymbol(contract, setSymbol) {
   contract.methods.symbol().call((error, result) => {
     if (error) {
-        console.error(error);
+      console.error(error);
     } else {
-        setSymbol(`\nSymbol: ${result}`);
+      setSymbol(result);
     }
-});
+  });
 }
+
+function getTotalSupply(contract, web3, setContractResponse) {
+  contract.methods.totalSupply().call((error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const resultInEther = web3.utils.fromWei(result, 'ether');
+      setContractResponse(resultInEther);
+    }
+  });
+}
+
 
 function getDecimals(contract, setDecimals) {
   contract.methods.decimals().call((error, result) => {
     if (error) {
-        console.error(error);
+      console.error(error);
     } else {
-      setDecimals(`\nDecimals: ${result}`);
+      setDecimals(result);
     }
-});
+  });
+}
+
+
+function getOwner(contract, setOwner) {
+  contract.methods.owner().call((error, result) => {
+    if (error) {
+      console.error(error);
+    } else {
+      setOwner(result);
+    }
+
+  });
 }
 
 export default App;
